c5f541cf73c261bdbc4aeda587f3eb5197e29257
Junichi Ito <jit@sonicgarden.jp>
2019-10-24 10:10:50 +0900

    Simplify spec

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -2,35 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
   describe "project create" do
-    let(:user) { FactoryBot.create(:user) }
+    let(:user) { FactoryBot.create(:user, first_name: 'Alice', last_name: 'Ito') }
 
-    before do
+    scenario 'user creates a new project' do
       sign_in_as(user)
-    end
+      click_link "New Project"
 
-    context "with project name and description" do
-      scenario 'user creates a new project' do
-        click_link "New Project"
-        fill_in "Name", with: "tama.rb"
-        fill_in "Description", with: "omotesando"
-        expect {
-          click_button "Create Project"
-        }.to change(user.projects, :count).by(1)
-        expect(page).to have_content "Project was successfully created"
-        expect(page).to have_content "tama.rb"
-      end
-    end
+      # 検証エラーが発生する場合
+      fill_in "Name", with: ""
+      fill_in "Description", with: "omotesando"
+      expect {
+        click_button "Create Project"
+      }.to_not change(user.projects, :count)
+      expect(page).to have_content "Name can't be blank"
 
-    context "with empty project name" do
-      scenario "user can't create a new project" do
-        click_link "New Project"
-        fill_in "Name", with: ""
-        fill_in "Description", with: "omotesando"
-        expect {
-          click_button "Create Project"
-        }.to_not change(user.projects, :count)
-        expect(page).to have_content "Name can't be blank"
-      end
+      # 正常な値を入力した場合
+      fill_in "Name", with: "tama.rb"
+      expect {
+        click_button "Create Project"
+      }.to change(user.projects, :count).by(1)
+      expect(page).to have_content "Project was successfully created"
+      expect(page).to have_content "tama.rb"
+      expect(page).to have_content "omotesando"
+      expect(page).to have_content "Owner: Alice Ito"
     end
   end
 end
